7245983e7605621d4c09eca0f30228b0bf4f31d3
WINDOWS-IEQD4KU\Aviha <Avihaihai737@gmail.com>
2016-10-29 11:45:52 -0700

    Added basic login form

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SimpleBlog.ViewModels;
 
 namespace SimpleBlog.Controllers
 {
@@ -11,6 +12,16 @@ namespace SimpleBlog.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+                return View(form);
+            return Content("The form is valid!");
+           
+           
         }
     }
 }
