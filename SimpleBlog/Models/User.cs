a36e15e36fb37ec9bbf0b4310db9712f990f04f2
WINDOWS-IEQD4KU\Aviha <Avihaihai737@gmail.com>
2016-10-30 22:58:28 -0700

    Implementing database driven authentication

--- SimpleBlog/Models/User.cs
+++ SimpleBlog/Models/User.cs
@@ -12,6 +12,19 @@ namespace SimpleBlog.Models
 
         // Basically here we're mapping in c# the table from mysql called users.
         // Every row in the table will get corresponding object User that will have the fields from the database.
+
+
+        // The fakehash is for the time attack on the website, let's say someone using a registered user in the system
+        // we can see by pulling up the Network the amount of time the database response with with the correct password.
+        // we want to throw in fakehash incase the user is exist.
+
+        private const int WorkFactor = 13;
+
+        public static void FakeHash()
+        {
+            BCrypt.Net.BCrypt.HashPassword("", WorkFactor);
+        }
+
         public virtual int Id { get; set; }
         public virtual string Username { get; set; }
         public virtual string Email { get; set; }
@@ -19,7 +32,13 @@ namespace SimpleBlog.Models
 
         public virtual void SetPassword(string password)
         {
-            PasswordHash = "IGNORE ME";
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public virtual bool CheckPassword(string password)
+        {
+            //this function will check if the typed in password is matched to the one we entered in the register form.
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
         }
     }
 
