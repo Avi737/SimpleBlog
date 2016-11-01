8a6872790f8a508798647f698ff90a41bec5f998
WINDOWS-IEQD4KU\Aviha <Avihaihai737@gmail.com>
2016-10-31 22:14:47 -0700

    Index action on post admin

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+using NHibernate.Linq;
+using SimpleBlog.Areas.Admin.ViewModels;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 {
@@ -11,9 +14,23 @@ namespace SimpleBlog.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController:Controller
     {
-        public ActionResult Index()
+        private const int PostsPerPage = 5;
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            // count of how many posts we have in the db , post that we on the current page.
+            // we need a get parameter to determine what page we're on, and default it to one if no parameters been specified.
+            var totalPostCount = Database.Session.Query<Post>().Count();
+            // getting the current page but first we need to know how many items per page
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * PostsPerPage) //if page is 1 we skip 0 items etc,..
+                .Take(PostsPerPage) // take 5(give us a slice of the pages based on what page we're on)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+                Posts =  new PageData<Post>(currentPostPage,totalPostCount,page,PostsPerPage)
+            });
         }
         
     }
